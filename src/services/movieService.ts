a7c33008268562c09945263519acbcd35d46ddc5
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-04-09 21:08:53 +0200

    add movieServise get method type

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,11 +3,15 @@ import type { Movie } from "../types/movie";
 
 const token = import.meta.env.VITE_TMDB_TOKEN;
 
+interface Response {
+  results: Movie[];
+}
+
 export const fetchMovies = async (
   query: string,
 ): Promise<Movie[] | undefined> => {
   try {
-    const response = await axios.get(
+    const response = await axios.get<Response>(
       `https://api.themoviedb.org/3/search/movie`,
       {
         params: {
@@ -18,6 +22,8 @@ export const fetchMovies = async (
         },
       },
     );
+    console.log(response);
+
     return response.data.results;
   } catch (error) {
     console.log(error);
